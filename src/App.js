05f91e6ff458d1d486b02be974762099c70481a5
Alexey Stitch <alexeyoffixial@yandex.ru>
2020-05-30 06:26:02 -0700

    TodoList process 40%

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import TodoList from "./TodoList";
+import TodoCreateForm from "./TodoCreateForm";
+
+const initialTodos = [
+    {id: 1, name: 'Test', done: false},
+    {id: 2, name: 'Test 2', done: true},
+    {id: 3, name: 'Test 3', done: true}
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todos, setTodos] = useState(initialTodos)
+
+    const onTaskCreate = (task) => {
+        console.log('APP' + task)
+        const updatedTodos = [...todos];
+        updatedTodos.push({id: Math.random(), name: task, done: false},)
+        setTodos(updatedTodos);
+    }
+
+    const onTaskDelete = (id) => {
+        console.log('APP delete' + id)
+        const updatedTodos = todos.filter(el => el.id !== id)
+        setTodos(updatedTodos)
+    }
+
+    const onTaskDone = (id) => {
+        const updatedTodos = todos.map(el => {
+            if (el.id === id) return { ...el, done: true }
+            else return el;
+        })
+
+        setTodos(updatedTodos);
+    };
+
+
+    return (
+        <div className="App">
+            <TodoList todos={todos} onTaskDelete={onTaskDelete} onTaskDone={onTaskDone}/>
+            <TodoCreateForm onTaskCreate={onTaskCreate}/>
+        </div>
+    );
 }
 
 export default App;
