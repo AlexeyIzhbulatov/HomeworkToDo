d9408dce22743b1339e5a2ab0c19c44335550101
Alexey Stitch <alexeyoffixial@yandex.ru>
2020-05-30 06:26:51 -0700

    TodoList process 40%

---
+++ src/TodoList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function TodoList(props) {
+
+
+    return (
+        <div className="todo">
+            {
+                props.todos.map(el => <li key={el.id}>
+                    {el.done && '✅'}
+                    {el.name}
+                    <button onClick={() => props.onTaskDelete(el.id)}>Delete</button>
+                    <button onClick={() => props.onTaskDone(el.id)}>Done</button>
+                </li>)
+            }
+        </div>
+    );
+}
+
+export default TodoList;
