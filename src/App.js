85f6c670fe1118c5f240dbb72e76ec3884e27346
AlexeyIzhbulatov <ouren@mail.ru>
2020-09-17 18:38:41 -0700

    setTodosLastJob

--- src/App.js
+++ src/App.js
@@ -3,51 +3,22 @@ import './App.css';
 import {v4 as uuidv4} from 'uuid';
 import TodoList from "./TodoList";
 
-const taski = [
-    {id: uuidv4(), nameTask: 'Name One', done: false, priority: 10},
-    {id: uuidv4(), nameTask: 'Name Two', done: false, priority: 20},
-    {id: uuidv4(), nameTask: 'Name Three', done: false, priority: 30},
+const initialTodos = [
+    {id: uuidv4(), name: 'Task1', done: false},
+    {id: uuidv4(), name: 'Task2', done: false},
+
 ]
 
 function App() {
 
-    const [tasks, setTasks] = useState(taski)
-
-    const onUpTask = (index) => {
-        if(index === 0) return
-        const unRealUpTask = tasks.map((el, i) => {
-            if (index === i) return tasks[index - 1];
-            if ((index - 1) === i) return tasks[index];
-            return el;
-        })
-        setTasks(unRealUpTask)
-    }
-
-
-    const onDownTask = (index) => {
-        if(index === 0) return
-        const unRealUpTask = tasks.map((el, i) => {
-            if (index === i) return tasks[index + 1];
-            if ((index + 1) === i) return tasks[index];
-            return el;
-        })
-        setTasks(unRealUpTask)
-    }
+const [task, setTask] = useState(initialTodos)
 
 
     return (
         <div className="App">
-            <TodoList tasks={tasks} onUpTask={onUpTask} onDownTask={onDownTask}/>
+            <TodoList task={task} arrowUp={arrowUp}/>
         </div>
     );
 }
 
 export default App;
-sadasdsad
-
-adadad
-
-dada
-
-
-sfsfsfsfsfsfs
